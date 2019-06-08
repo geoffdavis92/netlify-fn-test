1e8562a92ca6908a4daa8c3fc4278e56faab4f64
Geoff Davis <geoffdavis92@gmail.com>
2019-06-08 15:03:09 -0500

    add test_env_var to test env variables in Netlify functions

--- .netlify/functions/test.js
+++ .netlify/functions/test.js
@@ -1,8 +1,9 @@
 exports.handler = async function(event, context) {
   console.log(event);
   console.log(context);
+  const testEnvVar = test_env_var;
   return {
     statusCode: 200,
-    body: JSON.stringify({ event, context })
+    body: JSON.stringify({ event, context, testEnvVar })
   };
 };
