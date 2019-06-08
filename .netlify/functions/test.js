b69fb985c6c9feb08e6573999606fb1461273f12
Geoff Davis <geoffdavis92@gmail.com>
2019-06-08 15:05:07 -0500

    remove test

--- .netlify/functions/test.js
+++ .netlify/functions/test.js
@@ -1,9 +1,8 @@
 exports.handler = async function(event, context) {
   console.log(event);
   console.log(context);
-  const testEnvVar = test_env_var ? test_env_var : "None";
   return {
     statusCode: 200,
-    body: JSON.stringify({ event, context, testEnvVar })
+    body: JSON.stringify({ event, context })
   };
 };
