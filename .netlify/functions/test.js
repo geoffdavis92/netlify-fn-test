49e9efa1e23fb588682387bed56145a4b731cbe0
Geoff Davis <geoffdavis92@gmail.com>
2019-06-08 15:41:59 -0500

    test adding airtable import

--- .netlify/functions/test.js
+++ .netlify/functions/test.js
@@ -1,9 +1,16 @@
+const airtable = require("airtable");
+
 exports.handler = async function(event, context) {
   console.log(event);
   console.log(context);
   console.log(process.env);
   return {
     statusCode: 200,
-    body: JSON.stringify({ event, context, process_env: process.env })
+    body: JSON.stringify({
+      event,
+      context,
+      process_env: process.env,
+      airtableType: typeof airtable
+    })
   };
 };
