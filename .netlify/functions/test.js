5e7d0dc0e269d9043a6dedf714d1b1dd7788ae5c
Geoff Davis <geoffdavis92@gmail.com>
2019-06-08 15:07:00 -0500

    add process.env log

--- .netlify/functions/test.js
+++ .netlify/functions/test.js
@@ -1,6 +1,7 @@
 exports.handler = async function(event, context) {
   console.log(event);
   console.log(context);
+  console.log(process.env);
   return {
     statusCode: 200,
     body: JSON.stringify({ event, context })
