954b08920be2a166d32bb680da3e542b9063f5d5
Geoff Davis <geoffdavis92@gmail.com>
2019-06-08 15:04:15 -0500

    update test env var

--- .netlify/functions/test.js
+++ .netlify/functions/test.js
@@ -1,7 +1,7 @@
 exports.handler = async function(event, context) {
   console.log(event);
   console.log(context);
-  const testEnvVar = test_env_var;
+  const testEnvVar = test_env_var ? test_env_var : "None";
   return {
     statusCode: 200,
     body: JSON.stringify({ event, context, testEnvVar })
